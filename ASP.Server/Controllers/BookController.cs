21f268d32fc35bcfbb47bdbf0470d564e2da5747
Sylvain <37038164+syl9456@users.noreply.github.com>
2024-02-21 15:25:13 +0100

    Add Show and Update book

--- ASP.Server/Controllers/BookController.cs
+++ ASP.Server/Controllers/BookController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.Design;
 using System.Linq;
 using ASP.Server.Database;
 using ASP.Server.Models;
@@ -31,19 +32,67 @@ namespace ASP.Server.Controllers
             if (ModelState.IsValid)
             {
                 // Completer la création du genre avec toute les information nécéssaire que vous aurez ajoutez, et mettez la liste des gener récupéré de la base aussi
-                libraryDbContext.Add(new Book()
-                {
-                    Name = book.Name,
-                    Authors = book.Authors.Select(id => libraryDbContext.Authors.Find(id)).ToList(),
-                    Price = book.Price,
-                    Content = book.Content,
-                    Genres = book.Genres.Select(id => libraryDbContext.Genres.Find(id)).ToList()
-                });
+                var newBook = new Book
+                        {
+                            Name = book.Name,
+                            Authors = book.Authors.Select(id => libraryDbContext.Authors.Find(id)).ToList(),
+                            Price = book.Price,
+                            Content = book.Content,
+                            Genres = book.Genres.Select(id => libraryDbContext.Genres.Find(id)).ToList()
+                        };
+                libraryDbContext.Add(newBook);
                 libraryDbContext.SaveChanges();
+
+                return RedirectToAction("Show", new { id = newBook.Id });
             }
 
             // Il faut interoger la base pour récupérer tous les genres, pour que l'utilisateur puisse les slécétionné
             return View(new CreateBookViewModel()
+            {
+                AllAuthors = libraryDbContext.Authors, 
+                AllGenres = libraryDbContext.Genres
+            });
+        }
+        
+        public ActionResult Show([FromRoute]int id)
+        {
+            // Retourne la vue avec le modèle de livre
+            return View(libraryDbContext.Books.
+                Include(b => b.Authors).
+                Include(b => b.Genres).
+                Single(b => b.Id == id));
+        }
+
+        public ActionResult<UpdateBookViewModel> Update(UpdateBookViewModel book)
+        {
+            if (ModelState.IsValid)
+            {
+                // Récupérer le livre existant
+                var existingBook = libraryDbContext.Books
+                    .Include(b => b.Authors)
+                    .Include(b => b.Genres)
+                    .FirstOrDefault(b => b.Id == book.Id);
+
+                if (existingBook == null)
+                {
+                    // Gérer le cas où le livre n'est pas trouvé
+                    return NotFound();
+                }
+                
+                // Mettre à jour les propriétés de l'entité existante
+                existingBook.Name = book.Name;
+                existingBook.Price = book.Price;
+                existingBook.Content = book.Content;
+                existingBook.Authors = book.Authors.Select(id => libraryDbContext.Authors.Find(id)).ToList();
+                existingBook.Genres = book.Genres.Select(id => libraryDbContext.Genres.Find(id)).ToList();
+
+                // Sauvegarder les modifications
+                libraryDbContext.SaveChanges();
+
+                return RedirectToAction("Show", new { id = book.Id });
+            }
+
+            return View(new UpdateBookViewModel()
             {
                 AllAuthors = libraryDbContext.Authors, 
                 AllGenres = libraryDbContext.Genres
--- ASP.Server/ViewModels/UpdateBookViewModel.cs
+++ ASP.Server/ViewModels/UpdateBookViewModel.cs
@@ -1,20 +1,22 @@
-﻿using ASP.Server.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using ASP.Server.Models;
 
 namespace ASP.Server.ViewModels
 {
-    // Un ViewModel représente le contenu envoyé à la vu mais aussi le contenu récupéré a travers une requète HTTP:
-    // -> /api/1.0.0/xxx?prop1=123&prop2=azerty&prop3[]=0&prop3[]=1
-    public class CreateBookViewModel
+    public class UpdateBookViewModel
     {
         [Required]
+        public int Id { get; set; }
+        
         public String Name { get; set; }
+        
         // Ajouter ici tous les champ que l'utilisateur devra remplir pour ajouter un livre
         [Required(ErrorMessage = "You need at least 1 author"), MinLength(1)]
         public IEnumerable<int> Authors { get; set; }
-        public IEnumerable<Author> AllAuthors { get; init; }
+        public IEnumerable<Author> AllAuthors { get; set; }
+        
         public double Price { get; set; }
         public String Content { get; set; }
 
@@ -23,6 +25,6 @@ namespace ASP.Server.ViewModels
         public IEnumerable<int> Genres { get; set; }
 
         // Liste des genres a afficher à l'utilisateur
-        public IEnumerable<Genre> AllGenres { get; init; }
+        public IEnumerable<Genre> AllGenres { get; set; }
     }
 }
